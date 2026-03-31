3cbe0e429891c246ae0a71c80b9ee8aead424cf4
ea-nasir <118380166+ea-nasir@users.noreply.github.com>
2026-03-31 10:36:41 +0200

    Add InventoryService

---
+++ inventory-service/src/main/java/com/example/ordersystem/inventoryservice/messaging/OrderCreatedConsumer.java
@@ -0,0 +1,57 @@
+package com.example.ordersystem.inventoryservice.messaging;
+
+import com.example.ordersystem.inventoryservice.service.InventoryService;
+import com.example.ordersystem.sharedevents.InventoryRejectedEvent;
+import com.example.ordersystem.sharedevents.InventoryReservedEvent;
+import com.example.ordersystem.sharedevents.OrderCreatedEvent;
+import config.KafkaTopics;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Component
+public class OrderCreatedConsumer {
+    InventoryService inventoryService;
+    KafkaTemplate<String,Object> kafkaTemplate;
+
+    public OrderCreatedConsumer(InventoryService inventoryService, KafkaTemplate<String,Object> kafkaTemplate){
+        this.inventoryService = inventoryService;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    @KafkaListener(topics = KafkaTopics.ORDERS_CREATED,groupId = "inventory.service")
+    public void consume(OrderCreatedEvent event){
+        boolean reserved = inventoryService.reserve(
+                event.orderId(),
+                event.productId(),
+                event.quantity()
+        );
+
+        if (reserved) {
+            kafkaTemplate.send(
+                    "inventory.reserved",
+                    event.orderId(),
+                    new InventoryReservedEvent(
+                            UUID.randomUUID(),
+                            Instant.now(),
+                            event.orderId(),
+                            event.productId(),
+                            event.quantity()
+                    )
+            );
+        } else {
+            kafkaTemplate.send(
+                    "inventory.rejected",
+                    event.orderId(),
+                    new InventoryRejectedEvent(
+                            UUID.randomUUID(),
+                            Instant.now(),
+                            event.orderId(),
+                            "Insufficient stock or quantity < 1"
+                    )
+            );
+        }
+    }
+}
---
+++ inventory-service/src/main/java/com/example/ordersystem/inventoryservice/service/InventoryService.java
@@ -0,0 +1,34 @@
+package com.example.ordersystem.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class InventoryService {
+    private final Map<String,Integer> stock = new HashMap<>();
+    private final Map<String,Reservation> reservationMap = new HashMap<>();
+
+    public InventoryService(){
+        initStock(stock);
+    }
+
+    public synchronized boolean reserve(String orderId, String productId, int quantity){
+        int availableStock = stock.getOrDefault(productId,0);
+        if(quantity < 1 || availableStock < quantity){
+            return false;
+        }
+        stock.put(productId,availableStock - quantity);
+        reservationMap.put(orderId, new Reservation(productId,quantity));
+        return true;
+    }
+
+    private void initStock(Map<String,Integer> stock) {
+        stock.put("CHAIR", 10);
+        stock.put("DESK", 5);
+        stock.put("LAMP", 20); //todo: make config with properties
+    }
+
+    private record Reservation(String productId, int quantity){}
+}
